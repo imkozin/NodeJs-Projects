4944f6915d6914ba638b3d0785dd9a0767f2549f
imkozin <ivan.kozin@hotmail.com>
2023-07-23 01:53:17 +0300

    finished app

--- todoapp/config/db.js
+++ todoapp/config/db.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
+const password = encodeURIComponent("1q2w3e4r");
+const dbURI = `mongodb+srv://ivanko:${password}@cluster0.ij4lho7.mongodb.net/?retryWrites=true&w=majority`;
 
 async function start() {
     try {
-        await mongoose.connect('mongodb+srv://ivanko:<1q2w3e4r>@cluster0.rzfpwbi.mongodb.net/?retryWrites=true&w=majority', {
+        await mongoose.connect(dbURI, {
             useNewUrlParser: true,
-            useFindAndModify: false
-        })
-        app
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to the MongoDB database');
+    } catch (e) {
+        console.error('Error connecting to the database', e);
     }
 }
+
+module.exports = start;
--- todoapp/server.js
+++ todoapp/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const path = require('path');
+const start = require('./config/db.js');
+const todoRoutes = require('./routes/todos.js')
+// Connect to MongoDB
+start();
 
 const app = express();
 const hbs = exphbs.create({
@@ -7,13 +12,16 @@ const hbs = exphbs.create({
     extname: "hbs"
 })
 
-hbs.engine('hbs', hbs.engine)
+app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
- 
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 3000;
 
+app.use(todoRoutes);
+
 app.listen(PORT, () => {
-    console.log('Server is running');
-})
+    console.log(`Running server on PORT ${PORT}`);
+});
